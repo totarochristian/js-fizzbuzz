f6ceb60edbdc1259f090ef758c92646f7b149d73
totarochristian <totaro.christian.18@gmail.com>
2023-03-10 15:56:53 +0100

    Add the delivery of the project as comment in the script file

---
+++ js/script.js
@@ -0,0 +1,31 @@
+/*
+---------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------   Main Goal   ------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------
+Delivery:
+Write a program that prints the numbers from 1 to 100 in the console,
+but that for multiples of 3, "Fizz" stamps instead of the number and for multiples of 5, "Buzz".
+For numbers that are both multiples of 3 and 5, print “FizzBuzz”.
+*Before we start coding let's ask ourselves a few questions:*
+How do I know if one number is divisible by another?
+Have we seen anything special that we can use?
+
+Tips of the day:
+1. We always write before comments in Italian to understand what we want to do
+2. Let's try to imagine the operations we want our program to carry out as we would do it "by hand"
+---------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------   Bonus   --------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------
+- Create a container in the DOM , by adding (through the append() or innerHTML function) an html element with the correct 
+  number or string to display.
+- Apply different styles to the elements added to the DOM in BONUS 1, depending on whether the value entered is a number,
+  a fizz, a buzz or a fizzbuzz.
+  If you're out of style ideas, you might want to take a cue from the screenshot provided in the delivery.
+---------------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------   Program steps   ----------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------
+- 
+---------------------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------
+*/
